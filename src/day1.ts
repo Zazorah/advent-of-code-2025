4cf083bf8197026af7ef602db413254506541470
Zaz <zazorah@gmail.com>
2025-12-02 13:06:08 -0700

    Day 1 Answer

--- src/day1.ts
+++ src/day1.ts
@@ -1,13 +1,100 @@
 import { readLines } from "./utils";
 
+// Preliminary Thoughts:
+// - Having some class that represents the dial and can be 'turned' will make things easy here.
+
+interface DialResponse {
+  newValue: number;
+  zeroEncounters: number;
+}
+
+class Dial {
+  private currValue: number;
+  private dialMax: number;
+
+  constructor(startingValue: number = 50, maxValue: number = 100) {
+    this.currValue = startingValue;
+    this.dialMax = maxValue;
+  }
+
+  rotate(direction: "L" | "R", distance: number): DialResponse {
+    let zeroEncounters = 0;
+
+    // Increment in direction.
+    for (let i = 0; i < distance; i++) {
+      switch (direction) {
+        case "L":
+          if (this.currValue == 0) {
+            this.currValue += this.dialMax;
+          }
+
+          this.currValue--;
+          break;
+        case "R":
+          this.currValue++;
+          if (this.currValue == this.dialMax) {
+            this.currValue -= this.dialMax;
+          }
+          break;
+      }
+
+      // Catch Zero Occurrences
+      if (this.currValue == 0) {
+        zeroEncounters++;
+      }
+    }
+
+    return {
+      newValue: this.currValue,
+      zeroEncounters,
+    };
+  }
+
+  getValue() {
+    return this.currValue;
+  }
+}
+
 function part1(lines: string[]): number {
-  // Solve part 1
-  return 1;
+  let result = 0;
+  const dial = new Dial();
+
+  for (const line of lines) {
+    // Extract information.
+    const instruction = line.trim();
+    const direction = instruction[0] as "L" | "R";
+    const distance = parseInt(instruction.slice(1));
+
+    // Use our dial.
+    const { newValue } = dial.rotate(direction, distance);
+
+    // Check value and increment result.
+    if (newValue == 0) {
+      result += 1;
+    }
+  }
+
+  return result;
 }
 
 function part2(lines: string[]): number {
-  // Solve part 2
-  return 2;
+  let result = 0;
+  const dial = new Dial();
+
+  for (const line of lines) {
+    // Extract information.
+    const instruction = line.trim();
+    const direction = instruction[0] as "L" | "R";
+    const distance = parseInt(instruction.slice(1));
+
+    // Use our dial.
+    const { newValue, zeroEncounters } = dial.rotate(direction, distance);
+
+    // Increment Result
+    result += zeroEncounters;
+  }
+
+  return result;
 }
 
 const input = readLines(1); // Replace number with appropriate day.
